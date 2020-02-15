991e31c9857b6cb557f3eae3c3187c10b0976aa8
sovaz1997 <sovaz1997@yandex.ru>
2020-02-16 02:24:40 +0300

    fix: carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,11 +3,13 @@ const HALF_LIFE_PERIOD = 5730;
 
 module.exports = function dateSample(sampleActivity) {
   if(typeof sampleActivity === 'string') {
-    if(!/^[0-9]*[\.]{0,1}[0-9]*$/.test(sampleActivity)) {
+    if(!/^[0-9]*[\.]{0,1}[0-9]*[\.]{0,1}[0-9]*$/.test(sampleActivity)) {
       return false;
     }
 
-    const activity = parseFloat(sampleActivity);
+
+
+    const activity = parseFloat(sampleActivity.match(/^[0-9]*[\.]{0,1}[0-9]*/));
 
     if(activity <= 0 || activity >= 15) {
       return false;
