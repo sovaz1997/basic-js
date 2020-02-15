31874884aebc5305a2945f12908884d489e8c2bd
sovaz1997 <sovaz1997@yandex.ru>
2020-02-16 01:44:20 +0300

    feat: count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,13 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats( matrix ) {
+  let cnt = 0;
+  const re = /^\^\^$/
+  for(let i = 0; i < matrix.length; ++i) {
+    for(let j = 0; j < matrix[i].length; ++j) {
+      if(re.test(matrix[i][j])) {
+        ++cnt
+      }
+    }
+  }
+
+  return cnt;
 };
