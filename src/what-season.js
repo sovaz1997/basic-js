54637391cdcdb1cacb627d8f86ab5a0fddf73a90
sovaz1997 <sovaz1997@yandex.ru>
2020-02-16 13:59:59 +0300

    feat: what-season (part)

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,17 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if(typeof(date.getMonth) !== 'function') {
+    return false;
+  }
+
+  const month = date.getMonth()
+
+  if(month >= 2 && month < 5) {
+    return 'spring';
+  } else if(month >= 5 && month < 8) {
+    return 'summer';
+  } else if(month >= 8 && month < 11) {
+    return 'autumn';
+  }
+
+  return 'winter';
 };
