516db99c7a8ab617e1f0352c937f763cb5a43d96
sovaz1997 <sovaz1997@yandex.ru>
2020-02-16 22:38:00 +0300

    feat: modern syntax for pow

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,8 +1,8 @@
 module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
-    const turns = Math.pow(2, disksNumber);
+    const turns = 2 ** disksNumber - 1;
     
     return {
         turns: turns,
-        seconds: 3600 * turns / turnsSpeed
+        seconds: turns / turnsSpeed * 3600
     }
 }
