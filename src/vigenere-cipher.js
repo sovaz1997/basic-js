9b7b51759800ab868469e75cb911f8954563b27e
sovaz1997 <sovaz1997@yandex.ru>
2020-02-16 21:22:03 +0300

    feat: vigenere-cipher - base encryption

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,50 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor(direct=true) {
+        this.direct = direct;
+    }
+
+    symCode(sym) {
+        return sym.charCodeAt(0) - 'A'.charCodeAt(0);
+    }
+
+    shift(sym, length) {
+        const code = this.symCode(sym);
+        return String.fromCharCode((code + length) % 26 + 'A'.charCodeAt(0));
+    }
+
+    encrypt(string, key) {
+        string = string.toUpperCase();
+        key = key.toUpperCase();
+
+        if(string === undefined || key == undefined) {
+            throw new Error();
+        }
+
+        const res = [];
+        let spaceCount = 0;
+        for(let i = 0; i < string.length; ++i) {
+            if(string[i] >= 'A' && string[i] <= 'Z') {
+                res.push(this.shift(string[i], this.symCode(key[(i - spaceCount) % key.length])));
+            } else {
+                res.push(string[i]);
+            }
+
+            if(string[i] == ' ') {
+                spaceCount++;
+            }
+        }
+
+        return res.join('');
+        
     }
 
     decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+        if(string === undefined || key == undefined) {
+            throw new Error();
+        }
     }
+
+
 }
 
 module.exports = VigenereCipheringMachine;
