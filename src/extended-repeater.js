d951a4e1dd03ad6ba7936f699e07c595d6be4014
sovaz1997 <sovaz1997@yandex.ru>
2020-02-16 15:13:04 +0300

    feat: extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -8,24 +8,36 @@ module.exports = function repeater(str, options) {
         options.additionSeparator = '';
     }
 
+    if(options.repeatTimes === undefined) {
+        options.repeatTimes = 1;
+    }
+
     if(options.additionRepeatTimes === undefined) {
+        options.additionRepeatTimes = 1;
+    }
+
+    if(options.addition === undefined) {
         options.additionRepeatTimes = 0;
     }
 
     let res = '';
+
     for(let i = 0; i < options.repeatTimes; ++i) {
         res += str;
 
-        if(options.additionRepeatTimes  !== 0) {
-            res += (options.addition);
+        /*if(options.additionRepeatTimes  !== 0) {
+            for(let j = 0; j < )
+        }*/
 
-            if(options.additionRepeatTimes > 0) {
+        for(let j = 0; j < options.additionRepeatTimes; ++j) {
+            res += options.addition;
+
+            if(j < options.additionRepeatTimes - 1) {
                 res += options.additionSeparator;
             }
         }
-
         if(i < options.repeatTimes - 1) {
-            res += options.separator
+            res += options.separator;
         }
     }
 
