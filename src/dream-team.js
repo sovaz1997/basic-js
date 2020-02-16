9412adc9a90dc29e5ea8f68f76f1bdda1acd9624
sovaz1997 <sovaz1997@yandex.ru>
2020-02-16 13:39:24 +0300

    feat: more tests

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,19 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if(typeof(members) !== 'object') {
+    return false;
+  }
+  
+  return members
+  .filter((it) => {
+    return typeof(it) === 'string'
+  })
+  .map((it) =>  {
+    for(let i = 0; i < it.length; ++i) {
+      if(it[i] >= 'A' && it[i] <= 'Z') {
+        return it[i];
+      }
+    }
+  })
+  .sort().join('');
+
 };
